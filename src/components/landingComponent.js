23de46d1bc1f0c69dbed9fa0e338550c5c4fd466
binkpitch <pitchayutn@gmail.com>
2017-07-20 04:28:30 +0700

    add github link

--- src/components/landingComponent.js
+++ src/components/landingComponent.js
@@ -2,13 +2,14 @@ import React from 'react'
 import Box from 'grommet/components/Box'
 import Button from 'grommet/components/Button'
 import Heading from 'grommet/components/Heading'
+import SocialGithub from 'grommet/components/icons/base/SocialGithub'
 
 const landing = () => {
   return (
     <div className='landing'>
       <Box align='center' justify='center' full='vertical'>
         <Heading align='center'>
-          Hi, my name is Bink.
+          Hi, my name is <a target='_blank' href='https://github.com/binkpitch/'><SocialGithub size='medium' /> Bink</a>.
         </Heading>
         <Heading align='center' size='small'>
           I build web and mobile application.
@@ -24,6 +25,7 @@ const landing = () => {
               onClick={() => {}} />
           </Box>
         </Box>
+        <Box />
       </Box>
     </div>
   )
