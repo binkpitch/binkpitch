def5cc7b1d6bafd4f717f87ad91b56e6976f95c2
binkpitch <pitchayutn@gmail.com>
2017-07-20 23:49:22 +0700

    update no match page

--- src/pages/noMatchPage.js
+++ src/pages/noMatchPage.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import InjectSheet from 'react-jss'
+import Box from 'grommet/components/Box'
+import Meter from 'grommet/components/Meter'
+import Value from 'grommet/components/Value'
 
 const noMatchPage = (props) => {
   return (
-    <div />
+    <Box margin='large' style={{height: '80vh'}} align='center' justify='center'>
+      <Value value={404}
+        units='-->Not Found'
+        size='large' />
+      <Meter vertical={false}
+        size='large'
+        colorIndex='graph-2'
+        value={100} />
+    </Box>
   )
 }
 
-export default noMatchPage
+export default InjectSheet()(noMatchPage)
