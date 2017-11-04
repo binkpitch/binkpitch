671a4ef16bf5f5f75cd25ddff26c55454f0218b4
binkpitch <pitchayutn@gmail.com>
2017-11-05 00:23:44 +0700

    add missing components to storybook

--- src/stories/index.js
+++ src/stories/index.js
@@ -5,6 +5,11 @@ import { storiesOf } from '@storybook/react'
 import '../../node_modules/grommet-css'
 
 import HomePage from '../pages/homePage'
+import NoMatchPage from '../pages/noMatchPage'
+
+import Menu from '../components/menuComponent'
+import Footer from '../components/footerComponent'
+
 import Landing from '../components/landingComponent'
 import WhoAmI from '../components/whoAmIComponent'
 import WhatICode from '../components/whatICodeComponent'
@@ -13,6 +18,7 @@ import ContactMe from '../components/contactMeComponent'
 
 storiesOf('Page', module)
   .add('Home', () => <HomePage />)
+  .add('NoMatch', () => <NoMatchPage />)
 
 storiesOf('Component', module)
   .add('Landing', () => <Landing />)
@@ -20,3 +26,5 @@ storiesOf('Component', module)
   .add('WhatICode', () => <WhatICode />)
   .add('Showcase', () => <Showcase />)
   .add('ContactMe', () => <ContactMe />)
+  .add('Menu', () => <Menu />)
+  .add('Footer', () => <Footer />)
